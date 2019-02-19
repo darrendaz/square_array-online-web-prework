c90b195c6a36172de7574228cc2b8ad79c759d91
Darren L <darren.lou@gmail.com>
2019-02-19 01:02:00 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -4,7 +4,7 @@ def square_array(array)
   array_item = 0
   
   array.each do
-    new_array = array[array_item]**2
+    array[array_item] = array[array_item]**2
     array_item += 1
   end
     return new_array
